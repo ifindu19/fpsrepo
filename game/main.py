6928b2faae7942c9d4b38c886266b52832090a29
ifindu19 <121722574+ifindu19@users.noreply.github.com>
2023-01-31 16:05:02 +0000

    Update main.py

--- game/main.py
+++ game/main.py
@@ -9,7 +9,7 @@ Entity.default_shader = texture_blend_shader
 ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
 
 editor_camera = EditorCamera(enabled=False, ignore_paused=True)
-player = FirstPersonController(model='cube', texture='/gnome/chompski.png', z=-10, color=color.white, origin_y=-.5, speed=8)
+player = FirstPersonController(model='cube', texture='chompski.png', z=-10, color=color.white, origin_y=-.5, speed=8)
 player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
 
 gun = Entity(model='cube', parent=camera, position=(.5,-.25,.25), scale=(.3,.2,1), origin_z=-.5, color=color.red, on_cooldown=False)
@@ -28,7 +28,7 @@ def shoot():
         gun.on_cooldown = True
         gun.muzzle_flash.enabled=True
         from ursina.prefabs.ursfx import ursfx
-        ursfx([(0.0, 0.0), (0.1, 0.9), (0.15, 0.75), (0.3, 0.14), (0.6, 0.0)], volume=0.5, wave='/ak/shot.wav', speed=3.0)
+        ursfx([(0.0, 0.0), (0.1, 0.9), (0.15, 0.75), (0.3, 0.14), (0.6, 0.0)], volume=0.5, wave='shot.wav', speed=3.0)
         invoke(gun.muzzle_flash.disable, delay=.05)
         invoke(setattr, gun, 'on_cooldown', False, delay=.15)
         if mouse.hovered_entity and hasattr(mouse.hovered_entity, 'hp'):
